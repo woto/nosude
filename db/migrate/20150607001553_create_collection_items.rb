9f0a73571a72681ee3b4d5984e545542f41154c4
Ruslan Kornev <oganer@gmail.com>
2015-06-07 21:31:23 +0500

    Маленький баг с миграцией

--- db/migrate/20150607001553_create_collection_items.rb
+++ db/migrate/20150607001553_create_collection_items.rb
@@ -2,7 +2,7 @@ class CreateCollectionItems < ActiveRecord::Migration
   def change
     create_table :collection_items do |t|
       t.references :collection, index: true, foreign_key: true
-      t.references :collectionable, polymorphic: true, index: {name: 'collectionable'}, foreign_key: true
+      t.references :collectionable, polymorphic: true, index: {name: 'collectionable'}
 
       t.timestamps null: false
     end
